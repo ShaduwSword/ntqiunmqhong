29f4f6d67f681cbd4153813b0e7041b51be2f53d
ShaduwSword <thesword1312@gmail.com>
2025-02-03 22:24:04 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -4,13 +4,13 @@
 // Function to handle button click events
 function selectOption(option) {
     // Check which option was clicked
-    if (option === 'Cóa') {
+    if (option === 'yes') {
         // Flash rainbow colors
         flashRainbowColors(function() {
             document.getElementById('question').style.display = 'none'; // Hide the question
             displayCatHeart(); // Display the cat-heart.gif
         });
-    } else if (option === 'Hong') {
+    } else if (option === 'no') {
         // Change text on the "No" button to "You sure?"
         document.getElementById('no-button').innerText = 'Chắc tưa'; 
         // Increase font size of "Yes" button
